114cc02b861854c2323ae91a46dc011979d6ff5c
hyunhomon <hyunhomon@gmail.com>
2024-12-23 16:06:07 +0900

    feat: db

--- src/server/api/db.ts
+++ src/server/api/db.ts
@@ -1,6 +1,40 @@
 import { Express } from 'express';
+import { readFileSync, writeFileSync } from 'fs';
 
 const db = (app: Express, dbDir:string) => {
+    const read = (path: string) => {
+        try {
+            const data = readFileSync(`${dbDir}${path}`, 'utf-8');
+            return JSON.parse(data);
+        } catch (error) {
+            console.error('Error reading file:', error);
+            return {};
+        }
+    };
+    const write = (path: string, data: object) => {
+        try {
+            writeFileSync(`${dbDir}${path}`, JSON.stringify(data, null, 2));
+        } catch (error) {
+            console.error('Error writing file:', error);
+        }
+    };
+
+    const createDatabase = (name: string) => {};
+    const dropDatabase = (name: string) => {};
+    const showDatabase = (name: string) => {};
+    const createCollection = (name: string) => {};
+    const dropCollection = (name: string) => {};
+    const showCollection = (name: string) => {};
+
+    const insertOne = (document: Object) => {};
+    const insertMany = (documents: Object[]) => {};
+    const updateOne = (id: string, document: Object) => {};
+    const updateMany = (ids: string[], documents: Object[]) => {};
+    const deleteOne = (id: string) => {};
+    const deleteMany = (ids: string) => {};
+    const find = (query: Object, option: Object) => {};
+    const findOne = (query: Object, option: Object) => {};
+    const count = (query: Object, option: Object) => {};
 }
 
 export default db;
